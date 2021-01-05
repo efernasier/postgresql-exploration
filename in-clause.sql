10d0dc70a9b963bc0febd62967c5c9165c075ded
Eduardo Fernández <efernandez.ve@gmail.com>
2021-01-05 19:54:13 -0300

    Add some queries using IN

---
+++ in-clause.sql
@@ -0,0 +1,8 @@
+/*
+* DB: World
+* Table: city
+* Question: How many cities are in the district of Zuid-Holland, Noord-Brabant and Utrecht?
+*/
+
+-- SELECT * FROM city where district in ('Zuid-Holland', 'Noord-Brabant', 'Utrecht');
+
