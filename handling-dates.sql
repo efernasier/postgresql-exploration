301c833ffd6e71dd2ac455bd7dd35354e2833329
Eduardo Fernández <efernandez.ve@gmail.com>
2021-01-06 09:31:56 -0300

    add handling dates using utc

---
+++ handling-dates.sql
@@ -0,0 +1,10 @@
+-- Show TIMEZONE;
+-- set time zone 'UTC';
+
+-- '::' is for cast dates to other.
+-- select now()::date;
+
+-- Cast in european common format
+-- select CURRENT_DATE;
+-- select to_char( CURRENT_DATE, 'dd/mm/yyyy' );
+
